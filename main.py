9eb10ff09be707fb3c021e64372f3943db2f3dc7
Alba420p <albapaulina150@gmail.com>
2026-06-03 21:13:15 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+
+import matplotlib.pyplot as plt
+from models import calc_error, modelo_geom, modelo_circ
+
+
+def make_plot():
+    """
+    (Si no modificas esta cadena de texto lloro)
+    Si repites mucho tu código para
+    graficar puedes guardarlo en una función
+    """
+    ... # Esto significa implementación pendiente, lo puedes eliminar
+
+def main():
+    """
+    (Si no modificas esta cadena de texto lloro)
+    Aquí va el código, recuerda reutilizar el 
+    código que ya escribiste en otros archivos
+    """
+    ... # Esto significa implementación pendiente, lo puedes eliminar
+
+if __name__ == "__main__":
+    main()
